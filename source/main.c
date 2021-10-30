fd4f47cd9c94bc1a9e387b728f6e9ea8f0906bd5
dezem <dezem@users.noreply.github.com>
2021-10-30 23:01:43 +0200

    simplify ams_bpc

--- source/main.c
+++ source/main.c
@@ -1,17 +1,14 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
-
 #include <switch.h>
-#include "ams_bpc.h"
 
 int main(int argc, char **argv)
 {
     // Console init
     spsmInitialize();
-    
     // AMS safe reboot
     spsmShutdown(true);
-    
+    spsmExit();
     return 0;
 }
